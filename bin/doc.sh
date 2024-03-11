32ec02b45bb5666ad30637495efd378c5e92071e
Larpoux <larpoux@gmail.com>
2024-03-11 19:10:36 +0100

    doc.sh

--- bin/doc.sh
+++ bin/doc.sh
@@ -2,8 +2,9 @@
 
 VERSION=$1
 
-rm -r doc/pages/tau/api/* 2>/dev/null
-rm -r doc/_site *
+rm -r doc/pages/api 2>/dev/null
+rm -r doc/_site/* 2>/dev/null
+rm -r doc/api 2>/dev/null
 
 if [ ! -z "$VERSION" ]; then
         echo "Setting the tau version"
@@ -11,16 +12,80 @@ if [ ! -z "$VERSION" ]; then
         gsed -i  "s/^\( *version: \).*/\1$VERSION/"                                             doc/_data/sidebars/mydoc_sidebar.yml
 fi
 
+flutter clean
+flutter pub get
+
+flutter analyze
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit -1
+fi
+
+dart doc 
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit -1
+fi
+
+gsed -i  "0,/^  overflow: hidden;$/s//overflow: auto;/"  doc/api/static-assets/styles.css
+gsed -i  "s/^  background-color: inherit;$/  background-color: #2196F3;/" doc/api/static-assets/styles.css
+
+
+
+echo "Add Front matter on top of dartdoc pages"
+for f in $(find doc/api -name '*.html' )
+do
+        gsed -i  "1i ---" $f
+        gsed -i  "1i toc: true" $f
+        gsed -i  "1i ---" $f
+        gsed -i  "/^<script src=\"https:\/\/ajax\.googleapis\.com\/ajax\/libs\/jquery\/3\.2\.1\/jquery\.min\.js\"><\/script>$/d" $f
+done
+
+
+
+
+
+echo "Building Jekyll doc"
+cd doc
+rm home.md 2>/dev/null
+
+bundle config set --local path '~/vendor/bundle'
+bundle install
+bundle exec jekyll build
+
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit -1
+fi
+
+cd ..
+
+mv doc/api doc/_site
+cd doc/_site
+echo "Symbolic links of the API"
+echo "--------------------------"
+for dir in $(find api -type d)
+do
+        rel=`realpath --relative-to=$dir .`
+        echo "----- dir=$dir ----- rel=$rel"
+        for d in */ ; do
+            ln -s -v $rel/$d $dir
+        done
+done
+ln -s -v readme.html index.html
+cd ../..
+
+exit 0
+
 rm -rf example/build build
 tar czf _toto3.tgz extract
-cd doc
+cd doc/_site
 tar czf ../_toto.tgz *
-cd ..
-scp bin/doc2.sh canardoux@danku:/home/tau/bin
-scp _toto.tgz canardoux@danku:/home/tau
-scp _toto3.tgz canardoux@danku:/home/tau
-exit 0
-ssh canardoux@danku "bash /home/tau/bin/doc2.sh"
+cd ../..
+scp bin/doc2.sh canardoux@danku:/home/canardoux/bin
+scp _toto.tgz canardoux@danku:/home/canardoux
+scp _toto3.tgz canardoux@danku:/home/canardoux
+ssh canardoux@danku "bash /home/canardoux/bin/doc2.sh"
 #rm _toto.tgz _toto2.tgz _toto3.tgz 2>/dev/null
 
 echo 'E.O.J'
